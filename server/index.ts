37b32ef714676d0b6e38b3fbd0a9e2e675c8eaaf
mizanmail <mizanmail@example.com>
2026-01-01 13:56:31 +0530

    Configure project for Vercel deployment

--- server/index.ts
+++ server/index.ts
@@ -59,6 +59,8 @@ app.use((req, res, next) => {
   next();
 });
 
+export default app;
+
 (async () => {
   await registerRoutes(httpServer, app);
 
@@ -84,14 +86,16 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  if (process.env.NODE_ENV !== "production") {
+    const port = parseInt(process.env.PORT || "5000", 10);
+    httpServer.listen(
+      {
+        port,
+        host: "0.0.0.0",
+      },
+      () => {
+        log(`serving on port ${port}`);
+      },
+    );
+  }
 })();
